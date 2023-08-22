81c8e8363afa45a4aacf1e4c1d1fbd7f0d107983
koaminakligo-eaton <koaminakligo@eaton.com>
2023-08-22 14:11:56 -0400

    install specific packer to use

--- images/win/scripts/Installers/Install-Packer.ps1
+++
@@ -1,3 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco uninstall packer
-choco install packer --version=1.8.4 -y
