998744328cae4ab35e5264ce8a6eb95dee3a88e8
koaminakligo-eaton <koaminakligo@eaton.com>
2024-04-05 10:21:17 -0400

    LTK-23212 Install dependencies for tortoisesvn

--- images/win/scripts/Installers/Install-CommonUtils.ps1
+++ images/win/scripts/Installers/Install-CommonUtils.ps1
@@ -1,5 +1,8 @@
 $commonPackages = (Get-ToolsetContent).choco.common_packages
 
+choco install kb2919355 -y
+choco install kb2999226 -y
+
 foreach ($package in $commonPackages)
 {
     Choco-Install -PackageName $package.name -ArgumentList $package.args
